8b6b3154386977f1d5abdddcb4ca2ce1be537a61
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-01-24 11:46:38 +0900

    メッセージ機能 新規投稿機能 new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,15 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(msg_params)
+  end
+
+  private
+
+  def msg_params
+    params.require(:message).permit(:title, :content)
+  end
 end
