4b70c037d62c391ae96bd8a387bc881b3d6c0533
KENTA MITANI <mitani.kenta88@gmail.com>
2021-01-27 15:54:57 +0900

    一覧表示機能 index を実装＆ストロングパラメータのprivate修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
   def new
     @task = Task.new
   end
@@ -9,6 +12,6 @@ class TasksController < ApplicationController
 
   private
   def task_params
-   params.require(:taks).peremit(:title, :content)
+   params.require(:task).permit(:title, :content)
   end
 end
