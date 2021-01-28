c9ce187ff6466e88a6f85b25b35589c0905ea034
tajimakaito <Donan.fugarasa4792@icloud.com>
2021-01-28 23:13:51 +0900

    詳細表示機能 show を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all 
+    @users = User.all
   end
 
   def new
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
